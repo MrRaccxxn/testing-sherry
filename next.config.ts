92d4acc3b069bec38fc17b546dd039255f6899ec
MrRaccxxn <ayrtonskt@gmail.com>
2025-06-15 05:38:41 +0800

    fix deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        process: require.resolve("process/browser"),
+        buffer: require.resolve("buffer"),
+        stream: require.resolve("stream-browserify"),
+        util: require.resolve("util"),
+        assert: require.resolve("assert"),
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        os: require.resolve("os-browserify/browser"),
+        url: require.resolve("url"),
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
